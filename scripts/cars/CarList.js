feb7e0ea0a81ec115ce38602464597744d6bef1a
Steve Brownlee <chortlehoort@gmail.com>
2020-03-12 10:16:40 -0500

    Filtering now works

--- scripts/cars/CarList.js
+++ scripts/cars/CarList.js
@@ -2,6 +2,33 @@ import { useCars } from "./CarDataProvider.js"
 import { Car } from "./Car.js"
 
 const contentTarget = document.querySelector(".carList")
+const eventHub = document.querySelector("#container")
+
+eventHub.addEventListener("manufacturerChosen", customEvent => {
+    // Get all the cars
+    const carDataArray = useCars()
+
+    // Filter out the cars that are of the same make as what was chosen
+    const filteredCars = carDataArray.filter(
+        (carObject) => {
+            if (carObject.make === customEvent.detail.manufacturer) {
+                return true
+            }
+            return false
+        }
+    )
+
+    // Convert each object in the filtered array to a string
+    const carStringArray = filteredCars.map(
+        (carObject) => {
+            return Car(carObject)
+        }
+    )
+
+    // Join into a single string and inject into the DOM
+    contentTarget.innerHTML = carStringArray.join("")
+
+})
 
 export const CarList = () => {
     const carDataArray = useCars()
